eac76869375df4ae89675fa61b89149e755b018a
Andy Babay <andy.r.babay@nasa.gov>
2025-07-02 15:37:05 -0400

    Replacing docker-compose with docker compose

--- test/test.sh
+++ test/test.sh
@@ -7,9 +7,9 @@ mkdir -p utils
 ln -sf $(pwd)/src/shared/utils.py $(pwd)/utils/__init__.py
 
 if [ "$1" = "-d" ]; then
-    #docker-compose -f test/docker-compose.yaml up 
-    docker-compose -f test/docker-compose.yaml up --build
+    #docker compose -f test/docker-compose.yaml up 
+    docker compose -f test/docker-compose.yaml up --build
 else
-    docker-compose -f test/docker-compose.yaml down -v
-    docker-compose -f test/docker-compose.yaml up --build --abort-on-container-exit
+    docker compose -f test/docker-compose.yaml down -v
+    docker compose -f test/docker-compose.yaml up --build --abort-on-container-exit
 fi
